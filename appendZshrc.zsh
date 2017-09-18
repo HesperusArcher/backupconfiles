e2866f12e768bf39b5c7940e5bf5f6516cbb687b
Yuanchen Xie <yuanchen.gm@gmail.com>
2017-09-18 21:00:00 +0800

    + alias se in openSUSE + 20170918 bk

--- appendZshrc.zsh
+++ appendZshrc.zsh
@@ -1,11 +1,14 @@
 # append alias for oh-my-zsh ~/.zshrc
 
+ZSH_THEME="ys"
+
 # for rpm
 alias in="sudo dnf install"
 alias up="sudo dnf update"
 
 # for openSUSE
 alias in="sudo zypper in"
+alias se="sudo zypper se"
 alias up="sudo zypper up"
 alias zrm='sudo zypper rm -u'
 
