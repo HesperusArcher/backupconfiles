c4edcef1f4fed5cf9be9b70299502cb0c2aa6408
Yuanchen Xie <yuanchen.gm@gmail.com>
2017-10-02 18:19:43 +0800

    * source-fonts

--- Conf_OR_Inst-fonts-source.sh
+++ Conf_OR_Inst-fonts-source.sh
@@ -5,7 +5,6 @@ mkdir -p ~/.fonts/adobe-fonts/source-code-pro
 git clone --depth 1 --branch release https://github.com/adobe-fonts/source-code-pro.git ~/.fonts/adobe-fonts/source-code-pro
 # find ~/.fonts/ -iname '*.ttf' -exec echo \{\} \;
 fc-cache -f -v ~/.fonts/adobe-fonts/source-code-pro
+fc-cache -f -v ~/.fonts/adobe-fonts/source-han-serif
+fc-cache -f -v ~/.fonts/adobe-fonts/source-han-sans
 echo "finished installing"
-
-git clone git@github.com:syl20bnr/spacemacs.git ~/.emacs.d -b develop
-git clone git@github.com:erstern/dotspacemacsd.git ~/.spacemacs.d/
