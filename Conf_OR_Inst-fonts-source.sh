cb6cb81b5856193aa8873a5a3ba6b3bd6240ec83
Yuanchen Xie <yuanchen.gm@gmail.com>
2017-10-06 14:24:36 +0800

    * # fc-cache source-han

--- Conf_OR_Inst-fonts-source.sh
+++ Conf_OR_Inst-fonts-source.sh
@@ -5,6 +5,6 @@ mkdir -p ~/.fonts/adobe-fonts/source-code-pro
 git clone --depth 1 --branch release https://github.com/adobe-fonts/source-code-pro.git ~/.fonts/adobe-fonts/source-code-pro
 # find ~/.fonts/ -iname '*.ttf' -exec echo \{\} \;
 fc-cache -f -v ~/.fonts/adobe-fonts/source-code-pro
-fc-cache -f -v ~/.fonts/adobe-fonts/source-han-serif
-fc-cache -f -v ~/.fonts/adobe-fonts/source-han-sans
+# fc-cache -f -v ~/.fonts/adobe-fonts/source-han-serif
+# fc-cache -f -v ~/.fonts/adobe-fonts/source-han-sans
 echo "finished installing"
