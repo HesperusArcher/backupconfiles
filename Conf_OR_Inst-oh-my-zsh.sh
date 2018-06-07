b3c85b07809554c0bf59bb4596ea193eee1d349d
Yuanchen Xie <yuanchen.gm@gmail.com>
2018-06-07 20:38:04 +0800

    + plugin autosuggestions syntax-highlighting

--- Conf_OR_Inst-oh-my-zsh.sh
+++ Conf_OR_Inst-oh-my-zsh.sh
@@ -3,3 +3,6 @@
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 chsh -s /bin/zsh
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
