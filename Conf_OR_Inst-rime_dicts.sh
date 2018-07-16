903ae5594ea60b55fba4b5ed91c9f50fe84de7ca
Yuanchen Xie <yuanchen.gm@gmail.com>
2018-07-16 18:03:58 +0800

    + manjaro kde

--- Conf_OR_Inst-rime_dicts.sh
+++ Conf_OR_Inst-rime_dicts.sh
@@ -5,17 +5,15 @@ cd ~/.config/fcitx/rime/
 cp sync/stella/default.custom.yaml ./
 cp sync/stella/symbolstella.yaml ./
 
-cp sync/stella/luna_pinyin-cangjie5.schema.yaml ./
-cp sync/stella/cangjie5-M.schema.yaml ./
-
-cp sync/stella/combo_pinyin_kbcon-cangjie5.schema.yaml ./
-cp sync/stella/combo_zhung_kbcon-cangjie5.schema.yaml ./
+cp sync/stella/combo_pinyin_bt-wubi86.schema.yaml ./
+cp sync/stella/wubi86-triungkox.schema.yaml ./
+cp sync/stella/triungkoxsampheng.schema.yaml ./
+# cp sync/stella/zyenpheng.schema.yaml ./
 
 cp sync/stella/luna_pinyin.extended.dict.yaml ./
 cp sync/stella/luna_pinyin.hanyu.dict.yaml ./
 cp sync/stella/luna_pinyin.poetry.dict.yaml ./
 cp sync/stella/stella.dict.yaml ./
-cp sync/stella/zhung*.dict.yaml ./
+# cp sync/stella/zhung*.dict.yaml ./
 
-cp sync/stella/triungkoxsampheng.schema.yaml ./
-cp sync/stella/zyenpheng.schema.yaml ./
+cp sync/stella/weasel.custom.yaml ./
