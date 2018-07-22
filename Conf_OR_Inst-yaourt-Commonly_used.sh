2362eb07e6e2315625cdf255e62485cd5a528f2a
Yuanchen Xie <yuanchen.gm@gmail.com>
2018-07-22 10:01:23 +0800

    + aspell-en

--- Conf_OR_Inst-yaourt-Commonly_used.sh
+++ Conf_OR_Inst-yaourt-Commonly_used.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-yaourt -S tcl tk gcc-fortran rstudio-desktop emacs-git zsh git adobe-source-code-pro-fonts adobe-source-han-sans-cn-fonts adobe-source-han-serif-cn-fonts fcitx-rime fcitx-configtool kcm-fcitx fcitx-ui-light google-chrome-beta shadowsocks-qt5
+yaourt -S tcl tk gcc-fortran rstudio-desktop emacs-git zsh git adobe-source-code-pro-fonts adobe-source-han-sans-cn-fonts adobe-source-han-serif-cn-fonts fcitx-rime fcitx-configtool kcm-fcitx fcitx-ui-light google-chrome-beta shadowsocks-qt5 aspell-en
