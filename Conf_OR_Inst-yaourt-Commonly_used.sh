a8c173cdc7150b969bd19015d6c5f1dcdfb710d4
Yuanchen Xie <yuanchen.gm@gmail.com>
2018-08-20 10:04:39 +0800

    - xorg-xrandr

--- Conf_OR_Inst-yaourt-Commonly_used.sh
+++ Conf_OR_Inst-yaourt-Commonly_used.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-yaourt -S git vim zsh emacs-git nutstore fcitx-configtool fcitx-im fcitx-rime fcitx-ui-light kcm-fcitx adobe-source-code-pro-fonts adobe-source-han-sans-cn-fonts adobe-source-han-serif-cn-fonts aspell-en google-chrome-dev shadowsocks-qt5 redshift-qt rstudio-desktop-bin tcl tk perl-json gcc-fortran texlive-most anaconda xorg-xrandr
+yaourt -S git vim zsh emacs-git nutstore fcitx-configtool fcitx-im fcitx-rime fcitx-ui-light kcm-fcitx adobe-source-code-pro-fonts adobe-source-han-sans-cn-fonts adobe-source-han-serif-cn-fonts aspell-en google-chrome-dev shadowsocks-qt5 redshift-qt rstudio-desktop-bin tcl tk perl-json gcc-fortran texlive-most anaconda
