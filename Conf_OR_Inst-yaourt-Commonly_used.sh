e18907fe08e73fa0e1d3779e4730c86d9e50cffa
Yuanchen Xie <yuanchen.gm@gmail.com>
2018-10-14 10:54:40 +0800

    * can install anaconda use .sh or conda

--- Conf_OR_Inst-yaourt-Commonly_used.sh
+++ Conf_OR_Inst-yaourt-Commonly_used.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-yaourt -S git vim zsh emacs-git nutstore fcitx-configtool fcitx-im fcitx-rime fcitx-ui-light kcm-fcitx adobe-source-code-pro-fonts adobe-source-han-sans-cn-fonts adobe-source-han-serif-cn-fonts aspell-en google-chrome-dev shadowsocks-qt5 redshift-qt rstudio-desktop-bin tcl tk perl-json gcc-fortran texlive-most anaconda
+yaourt -S git vim zsh emacs-git nutstore fcitx-configtool fcitx-im fcitx-rime fcitx-ui-light kcm-fcitx adobe-source-code-pro-fonts adobe-source-han-sans-cn-fonts adobe-source-han-serif-cn-fonts aspell-en google-chrome-dev shadowsocks-qt5 redshift-qt rstudio-desktop-bin tcl tk perl-json gcc-fortran texlive-most nodejs-vmd
 
-# yaourt -S cytoscape
+# yaourt -S anaconda cytoscape
