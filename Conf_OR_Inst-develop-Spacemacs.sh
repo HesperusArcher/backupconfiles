6a748e39e347383fbce16f753aea0860b55173cb
Yuanchen Xie <yuanchen.gm@gmail.com>
2018-11-17 10:47:31 +0800

    + ess

--- Conf_OR_Inst-develop-Spacemacs.sh
+++ Conf_OR_Inst-develop-Spacemacs.sh
@@ -4,3 +4,4 @@ git clone git@github.com:syl20bnr/spacemacs.git ~/github/syl20bnr-spacemacs/ -b 
 # git clone git@github.com:erstern/dotspacemacsd.git ~/.spacemacs.d/
 git clone git@github.com:HesperusArcher/dotspacEmacs.git ~/.spacemacs.d/
 git clone git@github.com:HesperusArcher/dotemacs.d.git ~/.emacs.d/
+git clone git@github.com:emacs-ess/ESS.git ~/elisps/ess/
