cc89f35407be7bccbd9294fa4ac1bac532963828
Yuanchen Xie <yuanchen.gm@gmail.com>
2018-11-26 21:49:08 +0800

    + 20181126 bk

--- Conf_OR_Inst-yaourt-Commonly_used.sh
+++ Conf_OR_Inst-yaourt-Commonly_used.sh
@@ -3,3 +3,4 @@
 yaourt -S git vim zsh emacs-git nutstore fcitx-configtool fcitx-im fcitx-rime fcitx-ui-light kcm-fcitx adobe-source-code-pro-fonts adobe-source-han-sans-cn-fonts adobe-source-han-serif-cn-fonts aspell-en google-chrome-dev shadowsocks-qt5 redshift-qt rstudio-desktop-bin tcl tk perl-json gcc-fortran texlive-most texlive-langchinese nodejs-vmd
 
 # yaourt -S anaconda cytoscape
+# yaourt -S emboss phylip
