1358e900cf0d56c6e98c3fe1c3fc68f99736e6cb
Yuanchen Xie <yuanchen.gm@gmail.com>
2018-11-30 16:58:24 +0800

    + 20181130 bk

--- Conf_OR_Inst-develop-Spacemacs.sh
+++ Conf_OR_Inst-develop-Spacemacs.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-git clone git@github.com:syl20bnr/spacemacs.git ~/github/syl20bnr-spacemacs/ -b develop
+git clone git@github.com:syl20bnr/spacemacs.git ~/emacsdirs/syl20bnr-spacemacs/ -b develop
 # git clone git@github.com:erstern/dotspacemacsd.git ~/.spacemacs.d/
 git clone git@github.com:HesperusArcher/dotspacEmacs.git ~/.spacemacs.d/
 git clone git@github.com:HesperusArcher/dotemacs.d.git ~/.emacs.d/
-git clone git@github.com:emacs-ess/ESS.git ~/elisps/ess/
-
-mkdir ~/elisps/
-cp -r ~/nutstore/nutbk/lazycat-company-english-helper/ ~/elisps/
-git clone git@github.com:polymode/polymode.git ~/elisps/polymode/
-git clone git@github.com:polymode/poly-markdown.git ~/elisps/poly-markdown/
-git clone git@github.com:polymode/poly-noweb.git ~/elisps/poly-noweb/
-git clone git@github.com:polymode/poly-R.git ~/elisps/poly-R/
