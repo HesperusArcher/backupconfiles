041e00c8927206aee2e184fe9e24ff029f67b6f5
Yuanchen Xie <yuanchen.gm@gmail.com>
2018-12-06 09:24:31 +0800

    * 20181206bk * spacemacs as module

--- Conf_OR_Inst-develop-Spacemacs.sh
+++ Conf_OR_Inst-develop-Spacemacs.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-git clone git@github.com:syl20bnr/spacemacs.git ~/emacsdirs/syl20bnr-spacemacs/ -b develop
-# git clone git@github.com:erstern/dotspacemacsd.git ~/.spacemacs.d/
-git clone git@github.com:HesperusArcher/dotspacEmacs.git ~/.spacemacs.d/
+# git clone git@github.com:syl20bnr/spacemacs.git ~/emacsdirs/syl20bnr-spacemacs/ -b develop
 git clone git@github.com:HesperusArcher/dotemacs.d.git ~/.emacs.d/
+git clone git@github.com:HesperusArcher/dotspacEmacs.git ~/.spacemacs.d/
 cd ~/.emacs.d/
 git submodule update --init --recursive
+cd ~/.emacs.d/modules/syl20bnr-spacemacs/
+git checkout develop
